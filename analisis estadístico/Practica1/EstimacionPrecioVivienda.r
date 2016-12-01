7a7f1d5cdcba9061e4cc4f1a50be0fe924ff6898
amarino5 <a.marino.comesana@gmail.com>
2016-12-01 17:57:12 +0100

    ultimas versiones

--- analisis estadístico/Practica1/EstimacionPrecioVivienda.r
+++ analisis estadístico/Practica1/EstimacionPrecioVivienda.r
@@ -82,4 +82,100 @@ hist(modelo2$residuals)
 qqnorm(modelo2$residuals); qqline(modelo2$residuals,col=2)
 confint(modelo2,level=0.95)
 
+##vemos las correlaciones parciales de las variables para intuir aquellas que puedan ser colineales
+library(ggm)
+library(igraph)
+cor(houses)
+str(houses_cor)
+
+houses_cor <- houses
+houses_cor$date = as.numeric(houses_cor$date)
+houses_cor$bedrooms = as.numeric(houses_cor$bedrooms)
+houses_cor$floors = as.numeric(houses_cor$floors)
+houses_cor$waterfront = as.numeric(houses_cor$waterfront)
+houses_cor$view = as.numeric(houses_cor$view)
+houses_cor$condition = as.numeric(houses_cor$condition)
+houses_cor$grade = as.numeric(houses_cor$grade)
+houses_cor$yr_built = as.numeric(houses_cor$yr_built)
+houses_cor$yr_renovated = as.numeric(houses_cor$yr_renovated)
+houses_cor$zipcode = as.numeric(houses_cor$zipcode)
+cor(houses_cor)
+#library(parcor)
+#parcor(houses)
+
+modelo3=lm(log(price)~1,data=houses)
+summary(modelo3)
+plot(modelo3$residuals)
+smoothScatter(modelo3$residuals)
+hist(modelo3$residuals)
+qqnorm(modelo3$residuals); qqline(modelo3$residuals,col=2)
+confint(modelo3,level=0.95)
+
+##Al ejectarlo vemos que la variable sqft_basement tiene un N/A por lo que aporta nada
+modelo4=lm(log(price)~sqft_living+grade+sqft_above+sqft_living15,data=houses)
+summary(modelo4)
+plot(modelo4$residuals)
+smoothScatter(modelo4$residuals)
+hist(modelo4$residuals)
+qqnorm(modelo4$residuals); qqline(modelo4$residuals,col=2)
+confint(modelo4,level=0.95)
+
+##Ejecutamos un modelo con todas las variables para ver cuales aportan mas o son mas significativas
+##Como detectar el overfitting
+str(houses)
+modelo5=lm(log(price)~sqft_living+bedrooms+sqft_lot+floors+waterfront+view+condition+yr_built+yr_renovated+zipcode+grade+sqft_above+sqft_living15+sqft_lot15,data=houses)
+summary(modelo5)
+plot(modelo5$residuals)
+smoothScatter(modelo5$residuals)
+hist(modelo5$residuals)
+qqnorm(modelo5$residuals); qqline(modelo5$residuals,col=2)
+confint(modelo5,level=0.95)
+
+modelo6=lm(log(price)~sqft_living+sqft_lot+floors+waterfront+view+condition+zipcode+grade+sqft_above+sqft_living15,data=houses)
+summary(modelo6)
+plot(modelo6$residuals)
+smoothScatter(modelo6$residuals)
+hist(modelo6$residuals)
+qqnorm(modelo6$residuals); qqline(modelo6$residuals,col=2)
+confint(modelo6,level=0.95)
+
 #Vemos que el modelo mejora. Trataremos de incluir mas variables para ver si podemos reducir la variabilidad
+# Ademas intentaremos aplicar rlm 
+
+##### 8. Bloque de cálculo de estad�?sticos robustos #####
+
+alphas=seq(from=0,to=0.5,by=0.01)
+medias_recortadas=c()
+medias_winsorizadas=c()
+for (alpha in alphas){
+  medias_recortadas=c(medias_recortadas,mean(houses$price, trim=alpha))
+  medias_winsorizadas=c(medias_winsorizadas,MediaWinsor(houses$price,probs=c(alpha,1-alpha)))
+}
+
+Estimadores=data.frame(alphas)
+Estimadores$media=mean(houses$price)
+Estimadores$mediana=median(houses$price)
+Estimadores$recortada=medias_recortadas
+Estimadores$winsorizada=medias_winsorizadas
+
+Estimadores
+
+plot(Estimadores$recortada~Estimadores$alphas,ylim=c(min(Estimadores[,2:5]),max(Estimadores[,2:5])),col="red",type="l", main="representación de medias robustas", xlab="alfas",ylab="precio")
+lines(alphas,Estimadores$winsorizada,col="blue")
+lines(alphas,Estimadores$media,col="orange")
+lines(alphas,Estimadores$mediana,col="green")
+legend(x=mean(alphas),y=max(Estimadores[,2:5]),legend=c("media","mediana","media recortada","media winsorizada"),col=c("orange","green","red","blue"),pch=20)
+
+## -------------------------------------------------------------------------
+
+#Generamos el modelo de estadistica robusta y comparamos
+###No se porque no me deja ejecutar el modelo robusto con las mismas variables incluidas anteriormente
+modelo5=rlm(log(price)~1,data=houses)
+summary(modelo5)
+plot(modelo5$residuals)
+smoothScatter(modelo5$residuals)
+hist(modelo5$residuals)
+qqnorm(modelo5$residuals); qqline(modelo5$residuals,col=2)
+confint(modelo5,level=0.95)
+
+
